a3414ed4f5952f6b6783a89c15ce06905802772e
erick <erick@DESKTOP-CFH81S6>
2024-03-10 08:17:21 -0300

    Adicionar arquivos de projeto.

---
+++ api/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options) 
+            : base(options)
+        { }
+
+        public DbSet<Intervenientes> Intervenientes { get; set; }
+
+    }
+}
---
+++ api/Models/Intervenientes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    [Table("recruitment")]
+    public class Intervenientes
+    {
+        [Display(Name = "ID")]
+        [Column("ID")]
+        public int ID { get; set; }
+
+        [Display(Name = "Exportador")]
+        [Column("Exportador")]
+        public string Exportador { get; set; }
+    }
+}
